5b773f200ed50f58bedbfc18d11b3aa4581f2e1e
danswick <dan.swick@gmail.com>
2020-05-14 20:10:24 -0700

    add page descriptions and page descriptions component

--- docs/components/api/api-page-items.js
+++ docs/components/api/api-page-items.js
@@ -5,40 +5,57 @@ import ApiItemContents from './item-contents';
 import SectionWrapper from './section-wrapper';
 import Github from './github';
 import Feedback from '../feedback';
+import ApiPageDescription from './api-page-description';
 
 const apiFilterItems = require('../../util/api-filter-items.js');
 
 export default class ApiPageItems extends React.Component {
-    render() {
-        const pageDocSource = apiFilterItems(this.props.pageTitle);
-        const children = pageDocSource[0].members.static;
+    pageDocSource = apiFilterItems(this.props.pageTitle);
+    children = this.pageDocSource[0].members.static;
+    renderDescription = () => {
+        const description = this.pageDocSource[0].description || false;
+        if (description)
+            return <ApiPageDescription pageDescription={description} />;
+    };
 
+    render() {
         // There are 3 layouts based on the content:
         // 1. `SingleSection` (for Maps page)
         // 2. `GroupedSection` (for Properties and options page)
         // 3. `Section` (for all other pages)
 
-        if (children.length === 1) {
+        if (this.children.length === 1) {
             return (
-                <SingleSection
-                    key={children[0].name}
-                    {...children[0]}
-                    {...this.props}
-                />
+                <React.Fragment>
+                    {this.renderDescription()}
+                    <SingleSection
+                        key={this.children[0].name}
+                        {...this.children[0]}
+                        {...this.props}
+                    />
+                </React.Fragment>
             );
         }
-        return children.map(child => {
+        return this.children.map(child => {
             if (child.kind === 'note') {
                 return (
-                    <GroupedSection
-                        key={child.name}
-                        location={this.props.location}
-                        members={child.members}
-                        name={child.name}
-                    />
+                    <React.Fragment key={child.name}>
+                        {this.renderDescription()}
+                        <GroupedSection
+                            key={child.name}
+                            location={this.props.location}
+                            members={child.members}
+                            name={child.name}
+                        />
+                    </React.Fragment>
                 );
             } else {
-                return <Section key={child.name} {...this.props} {...child} />;
+                return (
+                    <React.Fragment key={child.name}>
+                        {this.renderDescription()}
+                        <Section key={child.name} {...this.props} {...child} />
+                    </React.Fragment>
+                );
             }
         });
     }
