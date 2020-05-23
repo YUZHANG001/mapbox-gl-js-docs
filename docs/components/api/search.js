004d18e4aa6bd18d2ba2b59f13e9336165d11efa
Katy DeCorah <decorah@mapbox.com>
2020-05-23 10:56:13 -0400

    tidy up search component

--- docs/components/api/search.js
+++ docs/components/api/search.js
@@ -8,38 +8,53 @@ import classnames from 'classnames';
 export default class ApiSearch extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { filter: '' };
+        this.state = { filter: '', results: [] };
     }
 
-    filterMembers = value => {
-        this.setState({ filter: value });
+    // set filter as user types
+    handleQuery = value => {
+        this.setState({ filter: value }, () => {
+            this.handleSearch();
+        });
     };
 
-    render() {
+    // perform search and return results
+    handleSearch = () => {
         const fuse = new Fuse(apiSearch, {
             keys: ['name', 'description', 'namespace'], // keys to perform search on
             threshold: 0.3 // slightly stricter
         });
-        const results = fuse.search(this.state.filter)
-            ? fuse.search(this.state.filter).map(result => result.item)
-            : [];
+        this.setState({
+            results: fuse.search(this.state.filter)
+                ? fuse.search(this.state.filter).map(result => result.item)
+                : []
+        });
+    };
+
+    // perform these functions when the users selects a menu item
+    handleResultClick = selection => {
+        // track click so we can guage usage of this feature
+        if (window && window.analytics) {
+            analytics.track('Searched GL JS API Reference', {
+                query: this.state.filter,
+                clicked: selection.path
+            });
+        }
+        // open selection in current window
+        window.open(selection.path, '_self');
+        // clear search
+        this.setState({ filter: '' });
+    };
+
+    render() {
+        const { results, filter } = this.state;
         return (
             <Downshift
                 id="api-reference-search"
-                inputValue={this.state.filter}
-                onChange={selection => {
-                    // track click
-                    if (window && window.analytics) {
-                        analytics.track('Searched GL JS API Reference', {
-                            query: this.state.filter,
-                            clicked: selection.path
-                        });
-                    }
-                    // open selection in current window
-                    window.open(selection.path, '_self');
-                }}
-                onInputValueChange={this.filterMembers}
-                itemToString={() => this.state.filter}
+                inputValue={filter}
+                onChange={selection => this.handleResultClick(selection)}
+                onInputValueChange={this.handleQuery}
+                itemToString={() => filter}
             >
                 {downshiftProps => {
                     const {
@@ -53,47 +68,17 @@ export default class ApiSearch extends React.Component {
                     return (
                         <div className="px24 none block-mm mb6">
                             <div className="relative">
-                                <label
-                                    className="hide-visually"
-                                    {...getLabelProps()}
-                                >
-                                    Search API Reference
-                                </label>
-                                <input
-                                    placeholder="Search API Reference"
-                                    className="input bg-white"
-                                    {...getInputProps({
-                                        onFocus: () => {
-                                            openMenu();
-                                        }
-                                    })}
+                                <Input
+                                    getInputProps={getInputProps}
+                                    openMenu={openMenu}
+                                    getLabelProps={getLabelProps}
                                 />
-                                {isOpen && this.state.filter && (
-                                    <div className="color-text shadow-darken25 round mt3 bg-white scroll-auto scroll-styled hmax240 absolute z4 w-full align-l py3">
-                                        {results.length > 0 ? (
-                                            <ul>
-                                                {results.map(
-                                                    (result, index) => (
-                                                        <Result
-                                                            getItemProps={
-                                                                getItemProps
-                                                            }
-                                                            index={index}
-                                                            result={result}
-                                                            key={index}
-                                                            downshiftProps={
-                                                                downshiftProps
-                                                            }
-                                                        />
-                                                    )
-                                                )}
-                                            </ul>
-                                        ) : (
-                                            <div className="px12">
-                                                No results
-                                            </div>
-                                        )}
-                                    </div>
+                                {isOpen && filter && (
+                                    <Results
+                                        results={results}
+                                        getItemProps={getItemProps}
+                                        downshiftProps={downshiftProps}
+                                    />
                                 )}
                             </div>
                         </div>
@@ -104,6 +89,62 @@ export default class ApiSearch extends React.Component {
     }
 }
 
+class Input extends React.Component {
+    render() {
+        const { getLabelProps, getInputProps, openMenu } = this.props;
+        return (
+            <React.Fragment>
+                <label className="hide-visually" {...getLabelProps()}>
+                    Search API Reference
+                </label>
+                <input
+                    placeholder="Search API Reference"
+                    className="input bg-white"
+                    {...getInputProps({
+                        onFocus: () => openMenu()
+                    })}
+                />
+            </React.Fragment>
+        );
+    }
+}
+
+Input.propTypes = {
+    getLabelProps: PropTypes.func.isRequired,
+    getInputProps: PropTypes.func.isRequired,
+    openMenu: PropTypes.func.isRequired
+};
+
+class Results extends React.Component {
+    render() {
+        const { results, getItemProps, downshiftProps } = this.props;
+        return (
+            <div className="shadow-darken25 round mt3 bg-white scroll-auto scroll-styled hmax180 absolute z4 w-full align-l py6">
+                {results.length > 0 ? (
+                    results.map((result, index) => (
+                        <Result
+                            getItemProps={getItemProps}
+                            index={index}
+                            result={result}
+                            key={index}
+                            downshiftProps={downshiftProps}
+                        />
+                    ))
+                ) : (
+                    <div className="px12 py3">No results</div>
+                )}
+            </div>
+        );
+    }
+}
+Results.propTypes = {
+    getItemProps: PropTypes.func.isRequired,
+    results: PropTypes.array.isRequired,
+    downshiftProps: PropTypes.shape({
+        highlightedIndex: PropTypes.number
+    })
+};
+
 class Result extends React.Component {
     render() {
         return (
@@ -112,9 +153,9 @@ class Result extends React.Component {
                     key: this.props.index,
                     item: this.props.result,
                     className: classnames(
-                        'py3 px12 link--gray cursor-pointer',
+                        'py6 px12 txt-s link--gray-dark cursor-pointer txt-break-word',
                         {
-                            'bg-gray-faint txt-bold':
+                            'bg-gray-faint':
                                 this.props.downshiftProps.highlightedIndex ===
                                 this.props.index
                         }
